2a321187bfbd0718e158e6dcc9fff1af08cf80cf
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2025-04-29 13:26:41 +0200

    graph-generated

--- onboarding_agent.py
+++ onboarding_agent.py
@@ -121,3 +121,27 @@ def generate(state: MessagesState):
     # This is the moment when we start the generation, see the graph in the sections below
     response = llm.invoke(prompt)
     return {"messages": [response]}
+
+
+graph_builder.add_node(query_or_respond)
+graph_builder.add_node(tools)
+graph_builder.add_node(generate)
+
+graph_builder.set_entry_point("query_or_respond")
+graph_builder.add_conditional_edges(
+    "query_or_respond",
+    tools_condition,
+    {END: END, "tools": "tools"},
+)
+
+graph_builder.add_edge("tools", "generate")
+graph_builder.add_edge("generate", END)
+
+# Init chat persistance
+memory = MemorySaver()
+
+# Compile with the peristance layer (similar to git logs)
+graph = graph_builder.compile(checkpointer=memory)
+
+# Specify an ID for the thread (similar to git branch)
+config = {"configurable": {"thread_id": "test_123"}}
