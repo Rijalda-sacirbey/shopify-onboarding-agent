86232e3a472637f9734a67dfead703c73e06ea4d
Rijalda-sacirbey <rsacirbego1@etf.unsa.ba>
2025-04-29 13:06:01 +0200

    scraper-added

--- onboarding_agent.py
+++ onboarding_agent.py
@@ -40,3 +40,10 @@ if not os.environ.get("MISTRAL_API_KEY"):
 embeddings = MistralAIEmbeddings(model="mistral-embed")
 
 vector_store = InMemoryVectorStore(embeddings)
+
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",), 
+    bs_kwargs=dict(parse_only=bs4.SoupStrainer(class_=("post-content", "post-title", "post-header"))), 
+)
+
+docs = loader.load()
